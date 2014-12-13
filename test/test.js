97df8af7da46ad4cc21e43a9155138d8cb4fe406
Vincent Bruijn <vebruijn@gmail.com>
2014-12-13 23:31:18 +0100

    added some basic regexes and some basic tests

---
+++ test/test.js
@@ -0,0 +1,113 @@
+var e = require('../lib/etymologiebank.js');
+
+var aa = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> \
+<html xmlns="http://www.w3.org/1999/xhtml" lang="nl"> \
+<head> \
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" /> \
+	<title>etymologiebank.nl</title> \
+	<link rel="stylesheet" href="http://www.etymologiebank.nl/css/etymologie.css" /> \
+</head> \
+<body> \
+<div id="wrapper"> \
+<div id="kop"> \
+<!-- <div id="logo"></div>--> \
+<div id="menu"> \
+<ul> \
+	<li><a href="http://www.etymologiebank.nl/">Home</a></li> \
+	<li><a href="http://www.etymologiebank.nl/zoek">Zoeken</a></li> \
+	<li><a href="http://www.etymologiebank.nl/werken">Werken</a></li> \
+	<li><a href="http://www.etymologiebank.nl/links">Links</a></li> \
+	<li><a href="http://www.etymologiebank.nl/partners">Partners</a></li> \
+	<li><a href="http://www.etymologiebank.nl/disclaimer">Disclaimer</a></li> \
+	<li><a href="http://www.etymologiebank.nl/colofon">Colofon</a></li> \
+</ul> \
+</div> \
+</div> \
+<div id="content"> \
+<table><tr> \
+<td id="left"><form name="Zoek lemma\'s" action="http://www.etymologiebank.nl/zoeken" method="post" accept-charset="UTF-8"> \
+<input id="searchfield-left" type="text" name="zoekexpressie" value="sabotage"> \
+<br> \
+<input id="searchBtn" type="submit" name="submit" value="Snelzoeken"> \
+</form><a href="http://www.onzetaal.nl/ot/meldaanap.php"><img id="banner" src="http://onzetaal.nl/uploads/editor/etybankbanner.gif"></img></a> \
+<p id="wiki"><a href="http://www.etymologiewebsite.nl/wiki/Hoofdpagina" target="new">Meehelpen? Ga naar etymologieWiki</a></p> \
+</td> \
+<td id="text">			<h2>Zoekresultaten</h2> \
+					<p><a href="http://www.etymologiebank.nl/trefwoord/sabotage">sabotage a </a></p> \
+                    <p><a href="http://www.etymologiebank.nl/trefwoord/sabotage">sabotage b </a></p> \
+				<div id="bron">Bronverwijzing: <br><i>Sijs, Nicoline van der (samensteller) (2010), Etymologiebank, op http://etymologiebank.nl/</i></div> \
+</td></tr></table> \
+</div><div id="voet">Hosted by Meertens Instituut</div> \
+</div> \
+</body> \
+</html>';
+
+var bb = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> \
+<html xmlns="http://www.w3.org/1999/xhtml" lang="nl"> \
+<head> \
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" /> \
+	<title>etymologiebank.nl</title> \
+	<link rel="stylesheet" href="http://www.etymologiebank.nl/css/etymologie.css" /> \
+</head> \
+<body> \
+<div id="wrapper"> \
+<div id="kop"> \
+<!-- <div id="logo"></div>--> \
+<div id="menu"> \
+<ul> \
+	<li><a href="http://www.etymologiebank.nl/">Home</a></li> \
+	<li><a href="http://www.etymologiebank.nl/zoek">Zoeken</a></li> \
+	<li><a href="http://www.etymologiebank.nl/werken">Werken</a></li> \
+	<li><a href="http://www.etymologiebank.nl/links">Links</a></li> \
+	<li><a href="http://www.etymologiebank.nl/partners">Partners</a></li> \
+	<li><a href="http://www.etymologiebank.nl/disclaimer">Disclaimer</a></li> \
+	<li><a href="http://www.etymologiebank.nl/colofon">Colofon</a></li> \
+</ul> \
+</div> \
+</div> \
+<div id="content"> \
+<table><tr> \
+<td id="left"><form name="Zoek lemma\'s" action="http://www.etymologiebank.nl/zoeken" method="post" accept-charset="UTF-8"> \
+<input id="searchfield-left" type="text" name="zoekexpressie" value=""> \
+<br> \
+<input id="searchBtn" type="submit" name="submit" value="Snelzoeken"> \
+</form><a href="http://www.onzetaal.nl/ot/meldaanap.php"><img id="banner" src="http://onzetaal.nl/uploads/editor/etybankbanner.gif"></img></a> \
+<p id="wiki"><a href="http://www.etymologiewebsite.nl/wiki/Hoofdpagina" target="new">Meehelpen? Ga naar etymologieWiki</a></p> \
+</td> \
+<td id="text">			 		<h2>ziekte (het ziek-zijn)</h2> \
+					<h3><a name="ewn"></a>M. Philippa e.a. (2003-2009) <i>Etymologisch Woordenboek van het Nederlands</i></h3> \
+							<div class="lemma"><p><b>ziek</b> <abbr title="bijvoeglijk naamwoord (adjectief)">bn.</abbr> ‘niet gezond’<br /><abbr title="Oudnederlands (voor 1200)">Onl.</abbr> <i>siek</i> ‘niet gezond’ in <i>thie geknisedon ande thie siechon lichamon</i> ‘de gekneusde en zieke lichamen’ [<abbr title="circa">ca.</abbr> 1100; Will.]; <abbr title="Middelnederlands (1200–1500)">mnl.</abbr> <i>siec</i> in <i>din mvoter is uil siec</i> ‘je moeder is heel ziek’ [1201-25; VMNW].<br /><abbr title="Oudsaksisch (voor 1100)">Os.</abbr> <i>siok</i> (<abbr title="Middelnederduits (1100–1500)">mnd.</abbr> <i>sēk</i>); <abbr title="Oudhoogduits (voor 1100)">ohd.</abbr> <i>sioh</i> (<abbr title="Nieuwhoogduits (na 1600)">nhd.</abbr> <i>siech</i>); <abbr title="Oudfries (voor 1550)">ofri.</abbr> <i>siāk</i> (<abbr title="Nieuwfries (na 1550)">nfri.</abbr> <i>siik</i>); <abbr title="Oudengels (voor 1100)">oe.</abbr> <i>sēok</i> (<abbr title="Nieuwengels (na 1700)">ne.</abbr> <i>sick</i>); <abbr title="Oudnoords (voor 1500) ">on.</abbr> <i>sjúkr</i> (<abbr title="Nieuwzweeds (na 1500)">nzw.</abbr> <i>sjuk</i>); <abbr title="Gotisch">got.</abbr> <i>siuks</i>; alle ‘ziek, zwak’, < <abbr title="Proto-Germaans">pgm.</abbr> <i>*seuka-</i>. Hierbij hoort een sterk werkwoord <i>*seukan-</i> ‘ziek zijn’, dat echter alleen is geattesteerd als <abbr title="Gotisch">got.</abbr> <i>siukan</i>. In andere talen is een zwak werkwoord ‘ziek zijn/worden’ van het <abbr title="bijvoeglijk naamwoord (adjectief)">bn.</abbr> afgeleid: <abbr title="Middelnederlands (1200–1500)">mnl.</abbr> <i>sieken</i> (zie onder); <abbr title="Oudhoogduits (voor 1100)">ohd.</abbr> <i>siohhēn</i>; <abbr title="Oudnoords (voor 1500) ">on.</abbr> <i>sjúkna</i>, <i>sokna</i>.<br />Verdere herkomst onbekend.<br />♦ <b>ziekte</b> <abbr title="zelfstandig naamwoord (substantief)">zn.</abbr> ‘het ziek zijn, kwaal, specifieke vorm van ziek zijn’. <abbr title="Middelnederlands (1200–1500)">Mnl.</abbr> <i>siecte</i> ‘het ziek zijn’ in <i>Meester P., die over him ghinc in siere ziecte</i> ‘dokter P., die hem behandelde toen hij ziek was’ [1343-44; MNW], ‘kwaal, specifieke vorm van ziek zijn’ in <i>Nyet allene die lijflike siecten, mer ooc mede die suucten der seden</i> [begin 15<sup>e</sup> eeuw; MNW]. Afleiding van <i>ziek</i> met het achtervoegsel → <b>-te</b>. In de concrete betekenis ‘kwaal, specifieke vorm van ziek zijn’ is in het Middelnederlands het verwante woord <i>sucht</i> gebruikelijker, zie → <b>zucht</b>. ♦ <b>zieken</b> <abbr title="werkwoord (verbum)">ww.</abbr> ‘zeuren, de sfeer bederven’. <abbr title="Middelnederlands (1200–1500)">Mnl.</abbr> <i>sieken</i> ‘ziek zijn, ziek worden’ [1240; Bern.], in <i>Soo wanneer een broeder begint te ziekene</i> [<abbr title="circa">ca.</abbr> 1450; MNW]; <abbr title="Vroegnieuwnederlands (1500–1700)">vnnl.</abbr> in <i>sieckende soldaten</i> [1645; iWNT]; <abbr title="Nieuwnederlands (na 1700)">nnl.</abbr> <i>zieken</i> overdrachtelijk ‘zich ergerlijk gedragen, vervelende opmerkingen maken’ [1984; Van Dale]. Afleiding van <i>ziek</i>. De letterlijke betekenis is verouderd.</p></div> \
+								<h3><a name="chrwb"></a>N. van der Sijs (2001), <i>Chronologisch Woordenboek</i></h3> \
+							<div class="lemma"><p><b>ziekte*</b> het ziek-zijn 1343-1344 [MNW]</p></div> \
+								<h3><a name="ewb"></a>P.A.F. van Veen en N. van der Sĳs (1997), <i>Van Dale Etymologisch woordenboek</i></h3> \
+							<div class="lemma"><p><b>ziekte</b><abbr class="erfwoord">*</abbr> [het ziek-zijn] {<i>siecte</i> 1343-1344} <b>middelnederduits</b> <i>seikte</i>, <b>oudfries</b> <i>siukte</i>; afgeleid van <b><i>ziek.</i></b></p></div> \
+								<h3><a name="donselaar"></a>J. van Donselaar (1989), <i>Woordenboek van het Surinaams-Nederlands</i></h3> \
+							<div class="lemma"><p><b>ziek’te</b>: <b>de ziekte</b>, (veroud.) lepra. <i>Wat deert u, Mathilde? vroeg de oude doktor. - Ik heb de ziekte! was het antwoord</i> (van Schaick 1866: 215; enige vindpl.). - Etym.: Vgl. de naam in S: takroe-siki (takroe = slecht; siki = ziekte). - Syn. boasie*, kokobe*. Zie ook: ex-Hansenpatiënt*.</p></div> \
+								<h3><a name="new"></a>J. de Vries (1971), <i>Nederlands Etymologisch Woordenboek</i></h3> \
+							<div class="lemma"><p><b>ziekte</b> znw. v., mnl. <i>siecte</i> (met <i>ie</i> onder invloed van <i>siec</i>) en <i>suucte</i>, mnd. <i>seikte</i>, <i>sūkte</i> ofri. <i>siūkte</i>, met verscherping van de dentaal van het suffix naast mnl. <i>sûkede</i>, mnd. <i>sēkede</i>, <i>sūkede</i>, mhd. <i>siuchede</i> < germ. <i>*siukiþō</i>. — Een ander abstractum is germ. <i>*siukīn</i>, vgl. oostmnl. mnd. <i>sūke</i>, ohd. <i>siuhhi</i> (nhd. <i>seuche</i>), ofri. <i>siūke</i>, got. <i>siukei</i>. — Afl. van <b><i>ziek</i></b>.</p></div> \
+								<h3><a name="fvw"></a>N. van Wijk (1936 [1912]), <i>Franck\'s Etymologisch woordenboek der Nederlandsche taal</i></h3> \
+							<div class="lemma"><p><b>ziekte</b> znw., mnl. <i>siecte</i> (met <i>ie</i> naar <i>siec</i>) en <i>suucte</i> v. = mnd. <i>seikte</i>, <i>sûkte</i> v. “ziekte”. De verhouding tot mnl. <i>sûkede</i>, mnd. <i>sêkede</i>, <i>sûkede</i>, mhd. <i>siuchede</i> v. “id.” is als bij <i><b>lengte</b></i>. Een ouder abstractum is ohd. <i>siuhhî</i> (nhd. <i>seuche</i>), mnd. oostmnl. <i>sûke</i>, got. <i>siukei</i> v. “id.”. Mnl. <i>siecheit</i> (<i>d</i>) v. “id.” is een ook reeds ohd. mnd. woord.</p></div> \
+								<h3><a name="fvws"></a>C.B. van Haeringen (1936), <i>Etymologisch woordenboek der Nederlandsche taal, Supplement</i></h3> \
+							<div class="lemma"><p><b>ziekte</b>. Ook ofri. <i>siûkte</i> v. ‘id.’. Bij ohd. <i>siuhhî</i> enz. adde: ofri. <i>siûke</i> v. ‘id. ‘.</p></div> \
+								<h3><a name="gtb"></a><i>Woordenboek der Nederlandsche taal</i> (WNT) & <i>Middelnederlandsch woordenboek</i> (MNW) & <i>Vroegmiddelnederlands woordenboek</i> (VMNW) & <i>Oudnederlands woordenboek</i> (ONW) – alle onderdeel van de <i>Geïntegreerde Taalbank</i> (GTB)</h3> \
+							<div class="lemma"><p>Zoek dit woord op in het <a href=\'http://gtb.inl.nl/iWDB/search?actie=results&amp;wdb=wnt%2Cvmnw%2Conw%2Cmnw&amp;uitvoer=HTML&amp;lemmodern="ziekte"\' target="_blank">WNT, MNW, VMNW, ONW</a>.</p></div> \
+							<p><a href="javascript:history.back()">Terug naar lijst</a></p> \
+	<div id="bron">Bronverwijzing: <br><i>Sijs, Nicoline van der (samensteller) (2010), Etymologiebank, op http://etymologiebank.nl/</i></div> \
+</td></tr></table> \
+</div><div id="voet">Hosted by Meertens Instituut</div> \
+</div> \
+</body> \
+</html>';
+
+
+/*e.search('sabotage', function(resp) {
+    console.log(resp);
+});
+
+e.show('ziekte', function(resp) {
+    console.log(resp);
+});*/
+
+console.log(e.searchToJson('sabotage', aa));
+
+console.log(e.showToJson('ziekte', bb));
