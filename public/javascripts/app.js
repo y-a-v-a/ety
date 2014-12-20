94fe68e60cde22a85d673cdf1534273bf22b06ad
Vincent Bruijn <vebruijn@gmail.com>
2014-12-20 15:05:32 +0100

    fine tuning

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,19 +1,30 @@
-var codeIndexApp = angular.module('etyApp', []);
+var etyApp = angular.module('etyApp', []);
 
-codeIndexApp.controller('etyCtrl', ['$scope', '$http', function($scope, $http) {
+etyApp.controller('etyCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.fetch = function() {
-        $http.get('/query/' + encodeURIComponent($scope.text))
-        .success(function(data) {
-            $scope.term = data.term
-            $scope.terms = data.results;
-        });
+        $scope.sources = [];
+        if (typeof $scope.text !== 'undefined' && $scope.text.length > 2) {
+            $http.get('/query/' + encodeURIComponent($scope.text))
+            .success(function(data) {
+                $scope.term = data.term
+                $scope.terms = data.results;
+            });
+        }
     }
     
     $scope.show = function(ev) {
         ev.preventDefault();
-        $http.get('/show/' + encodeURIComponent($scope.text))
+        var word = ev.target.href.replace(/^.*\//,'');
+        
+        $http.get('/show/' + encodeURIComponent(word))
         .success(function(data) {
             $scope.sources = data.sources;
         });
+    }
+}]);
+
+etyApp.filter("sanitize", ['$sce', function($sce) {
+    return function(htmlCode){
+        return $sce.trustAsHtml(htmlCode);
     }
 }]);
