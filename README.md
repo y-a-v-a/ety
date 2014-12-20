dd705d2502228df85a2725e4de415f06eb208ee5
Vincent Bruijn <vebruijn@gmail.com>
2014-12-20 15:51:10 +0100

    added meta data and styling

---
+++ README.md
@@ -0,0 +1,17 @@
+Ety
+===
+
+Ety is the, in my opinion, more user friendly www.etymologiebank.nl. I like to research the etymology of Dutch words. I do like the contents of www.etymologiebank.nl but I don't like their way of searching for a word. So I made it easier to navigate. This app is a wrapper around www.etymologiebank.nl and a scraper in one. To not bother them with continuous (re)searches of myself, I decided to scrape it with each newly searched word and store it in a mongodb.
+
+Technologies used:
+
+* Angular
+* Mongoose / Mongodb
+* NodeJS
+* jsdom
+* Express with Jade
+
+License
+This code is licensed via MIT license, all content from www.etymologiebank.nl licensed as metioned on the site.
+
+Vincent Bruijn
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,24 +2,28 @@ var etyApp = angular.module('etyApp', []);
 
 etyApp.controller('etyCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.fetch = function() {
-        $scope.sources = [];
+        $scope.sources = undefined;
         if (typeof $scope.text !== 'undefined' && $scope.text.length > 2) {
             $http.get('/query/' + encodeURIComponent($scope.text))
             .success(function(data) {
                 $scope.term = data.term
                 $scope.terms = data.results;
             });
+        } else {
+            $scope.term = undefined;
+            $scope.terms = undefined;
         }
     }
     
     $scope.show = function(ev) {
         ev.preventDefault();
-        var word = ev.target.href.replace(/^.*\//,'');
-        
-        $http.get('/show/' + encodeURIComponent(word))
-        .success(function(data) {
-            $scope.sources = data.sources;
-        });
+        var word = ev.target.href.replace(/^.*\//, '');
+        if (word.length > 1) {
+            $http.get('/show/' + encodeURIComponent(word))
+            .success(function(data) {
+                $scope.sources = data.sources;
+            });
+        }
     }
 }]);
 
